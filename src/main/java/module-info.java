a49d572ba12677ab13353604cc23c389f9bde2d4
zNeht <NinjarossoYT.2005@gmail.com>
2025-12-06 17:53:05 +0100

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.uninaswapoobd2425 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.uninaswapoobd2425 to javafx.fxml;
+    exports com.example.uninaswapoobd2425;
+}
