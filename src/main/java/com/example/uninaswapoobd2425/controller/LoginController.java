a3fdbf1fbb31ec6968a1852ef20e1b6d093c9382
zNeht <NinjarossoYT.2005@gmail.com>
2025-12-11 00:16:58 +0100

    init commit

--- src/main/java/com/example/uninaswapoobd2425/controller/LoginController.java
+++ src/main/java/com/example/uninaswapoobd2425/controller/LoginController.java
@@ -1,14 +1,20 @@
 package com.example.uninaswapoobd2425.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.button;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.uninaswapoobd2425 {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.example.uninaswapoobd2425 to javafx.fxml;
     exports com.example.uninaswapoobd2425;
