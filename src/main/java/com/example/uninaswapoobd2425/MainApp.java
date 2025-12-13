9602d264f4504c29607c45d25a4125819e4b0a6b
zNeht <NinjarossoYT.2005@gmail.com>
2025-12-13 21:14:46 +0100

    init commit

--- src/main/java/com/example/uninaswapoobd2425/MainApp.java
+++ src/main/java/com/example/uninaswapoobd2425/MainApp.java
@@ -3,17 +3,38 @@ package com.example.uninaswapoobd2425;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class MainApp extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = fxmlLoader.load();
+
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
+        Scene scene = new Scene(root);
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+
         stage.setResizable(false);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/uninaswapoobd2425/controller/LoginController.java
+++ src/main/java/com/example/uninaswapoobd2425/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.example.uninaswapoobd2425.controller;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 import java.io.File;
@@ -15,6 +22,40 @@ import java.sql.Statement;
 
 
 public class LoginController {
+    @FXML
+    void handleClose(ActionEvent event) {
+        javafx.application.Platform.exit();
+    }
 
+    @FXML
+    void handleMinimize(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+
+        Node root = source.getScene().getRoot();
+
+        FadeTransition fade = new FadeTransition(Duration.millis(250), root);
+        fade.setFromValue(1.0);
+        fade.setToValue(0.0);
+
+        ScaleTransition scale = new ScaleTransition(Duration.millis(250), root);
+        scale.setFromX(1.0);
+        scale.setToX(0.8);
+        scale.setFromY(1.0);
+        scale.setToY(0.8);
+
+
+        ParallelTransition animation = new ParallelTransition(fade, scale);
+
+        animation.setOnFinished(e -> {
+            stage.setIconified(true);
+
+            root.setOpacity(1.0);
+            root.setScaleX(1.0);
+            root.setScaleY(1.0);
+        });
+
+        animation.play();
+    }
 
 }
