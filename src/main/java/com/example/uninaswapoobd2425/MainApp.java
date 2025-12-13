2157041ee857b7ba0d611d94a1e6683ce78e5a3f
zNeht <NinjarossoYT.2005@gmail.com>
2025-12-13 17:42:29 +0100

    init commit

--- src/main/java/com/example/uninaswapoobd2425/MainApp.java
+++ src/main/java/com/example/uninaswapoobd2425/MainApp.java
@@ -11,7 +11,8 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/uninaswapoobd2425/controller/LoginController.java
+++ src/main/java/com/example/uninaswapoobd2425/controller/LoginController.java
@@ -6,7 +6,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.button;
+
 
 import java.io.File;
 import java.net.URL;
